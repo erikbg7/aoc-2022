b1110872c9fb534cd6818f2723f2c76fc6458afe
erikbg7 <erik.bg7@gmail.com>
2022-12-10 00:15:38 +0100

    🕘 Day 9

--- Day9/index.js
+++ Day9/index.js
@@ -10,11 +10,11 @@ const DIRECTIONS = {
   R: {y: 0, x: 1},
 }
 
-function areAdjacent(x1, y1, x2, y2) {
-  return Math.abs(x2 - x1) <= 1 && Math.abs(y2 - y1) <= 1;
+function areAdjacent(knot1, knot2) {
+  return Math.abs(knot2.x - knot1.x) <= 1 && Math.abs(knot2.y - knot1.y) <= 1;
 }
 
-function solveChallenge() {
+function solveFirstChallenge() {
   const visitedCells = new Set();
   visitedCells.add("0,0");
 
@@ -28,7 +28,7 @@ function solveChallenge() {
       const nextX = head.x + DIRECTIONS[direction].x;
       const nextY = head.y + DIRECTIONS[direction].y;
       
-      if(!areAdjacent(tail.x, tail.y, nextX, nextY)) {
+      if(!areAdjacent(tail, { x: nextX, y: nextY })) {
         tail.x = head.x;
         tail.y = head.y;
         visitedCells.add(`${tail.x},${tail.y}`);
@@ -42,4 +42,48 @@ function solveChallenge() {
   return visitedCells.size;
 }
 
-console.log(solveChallenge()); // 6209
+
+function solveSecondChallenge() {
+  const visitedCells = new Set();
+  visitedCells.add("0,0");
+
+  const knots = new Array(10).fill(1).map(() => ({x: 0, y: 0}));
+
+  commands.forEach(command => {
+    const [direction, distance] = command.split(" ");
+    
+    for(let i = 0; i < parseInt(distance); i++) {
+      for(let j = 0; j < knots.length; j++) {
+
+        const isHead = j === 0;
+        const isTail = j === (knots.length - 1);
+
+        const leadingKnot = knots[j - 1];
+        const currentKnot = knots[j];
+
+        if(isHead) {
+          knots[j].x += DIRECTIONS[direction].x;
+          knots[j].y += DIRECTIONS[direction].y;
+          continue;
+        }
+
+        if(!areAdjacent(currentKnot, leadingKnot)) {          
+          const diffX = leadingKnot.x - currentKnot.x;
+          const diffY = leadingKnot.y - currentKnot.y;
+
+          knots[j].x += diffX/Math.abs(diffX) || 0;
+          knots[j].y += diffY/Math.abs(diffY) || 0;
+          
+          if(isTail) {  
+            visitedCells.add(`${currentKnot.x},${currentKnot.y}`);
+          }
+        } 
+      }
+    }
+  });  
+  return visitedCells.size;
+}
+
+
+console.log(solveFirstChallenge()); // 6209
+console.log(solveSecondChallenge()); // 2460
